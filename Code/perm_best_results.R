faaf42dbc2e909c7886106a6c44a0851ac08e162
annatyler <anna.l.tyler@gmail.com>
2022-04-04 16:02:01 -0400

    udpates

--- Code/perm_best_results.R
+++ Code/perm_best_results.R
@@ -1,17 +1,23 @@
 perm_best_results <- function(perm_grid, pval.thresh = 0.05, 
-    cor.diff.thresh = 0, plot.results = TRUE){
+    cor.diff.thresh = 0, return.top.only = FALSE, 
+    plot.results = TRUE, row.text.shift = -0.25, col.text.shift = 0){
 
     pass.p <- which(perm_grid$p <= pval.thresh)
     cor.diff <- perm_grid$Cor - perm_grid$Perm.Cor
     pass.cor <- which(cor.diff > cor.diff.thresh)
-    best.penalty.idx <- intersect(pass.p, pass.cor)
+    
+    if(return.top.only){
+        best.penalty.idx <- intersect(which.max(perm_grid$Cor), pass.p)
+    }else{
+        best.penalty.idx <- intersect(pass.p, pass.cor)
+    }
     
     nx <- nrow(cor.diff)
     nz <- ncol(cor.diff)    
 
     #which rows correspond to the indices found
     best.penalty.x <- best.penalty.idx %% nx
-    best.penalty.x[which(best.penalty.x == 0)] <- 1
+    best.penalty.x[which(best.penalty.z == 0)] <- nx
 
     #which columns correspond to the indices found
     best.penalty.z <- ceiling(best.penalty.idx/nx)
@@ -25,29 +31,33 @@ perm_best_results <- function(perm_grid, pval.thresh = 0.05,
         par(mfrow = c(2,2), mar = c(1,2,2,1))
         imageWithText(round(perm_grid$Cor, 2), col.scale = "blue", 
         main = "Correlations of Identified Components", 
-        col.text.rotation = 0, row.text.shift = -0.25)
-        points(x = best.penalty.x+star.nudge, 
-        y = nz - best.penalty.z + 1 + star.nudge, 
+        col.text.rotation = 0, row.text.shift = row.text.shift,
+        col.text.shift = col.text.shift)
+        points(x = best.penalty.z+star.nudge, 
+        y = nx - best.penalty.x + 1 + star.nudge, 
         pch = "*", col = "red", cex = 1)
         
         imageWithText(round(perm_grid$Perm.Cor, 2), col.scale = "red", 
         main = "Correlations of Permuted Components", 
-        col.text.rotation = 0, row.text.shift = -0.25)
-        points(x = best.penalty.x + star.nudge, 
-        y = nz - best.penalty.z + 1 + star.nudge, 
+        col.text.rotation = 0, row.text.shift = row.text.shift,
+        col.text.shift = col.text.shift)
+        points(x = best.penalty.z+star.nudge, 
+        y = nx - best.penalty.x + 1 + star.nudge, 
         pch = "*", col = "blue", cex = 1)
-        
+
         imageWithText(round(cor.diff, 2), col.scale = "purple", 
         main = "Difference in Correlations", 
-        col.text.rotation = 0, row.text.shift = -0.25)
-        points(x = best.penalty.x + star.nudge, 
-        y = nz - best.penalty.z + 1 + star.nudge, 
+        col.text.rotation = 0, row.text.shift = row.text.shift,
+        col.text.shift = col.text.shift)
+        points(x = best.penalty.z+star.nudge, 
+        y = nx - best.penalty.x + 1 + star.nudge, 
         pch = "*", col = "red", cex = 1)
         
         imageWithText(perm_grid$p, col.scale = "green", 
-        main = "P values", col.text.rotation = 0, row.text.shift = -0.25)
-        points(x = best.penalty.x + star.nudge, 
-        y = nz - best.penalty.z + 1 + star.nudge, 
+        main = "P values", col.text.rotation = 0, row.text.shift = row.text.shift,
+        col.text.shift = col.text.shift)
+        points(x = best.penalty.z+star.nudge, 
+        y = nx - best.penalty.x + 1 + star.nudge, 
         pch = "*", col = "red", cex = 1)
     }
 
