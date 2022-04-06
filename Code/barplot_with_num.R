f1e04245891b2045f767fee5db48631184254e4a
annatyler <anna.l.tyler@gmail.com>
2022-04-06 10:45:54 -0400

    updates

--- Code/barplot_with_num.R
+++ Code/barplot_with_num.R
@@ -3,17 +3,25 @@
 
 
 barplot_with_num <- function(values, xlab = "", ylab = "", main = "", col = "gray",
-    cex = 1, text.gap = 0.1, text.shift = 0.05, horiz = FALSE, names = NULL, las = 1){
+    cex = 1, text.gap = 0.1, text.shift = 0.05, horiz = FALSE, names = NULL, las = 1, 
+    ylim = NULL){
 
     plot.range <- max(values, na.rm = TRUE) - min(values, na.rm = TRUE)
     coords <- barplot(values, plot = FALSE)
 
     if(!horiz){
-        ylim <- c(0, max(values)+(plot.range*text.gap))
-        xlim <- c(0, max(coords))
-    }else{
-        ylim <- c(0, max(coords))
-        xlim <- c(0, max(values)+(plot.range*text.gap))
+        if(is.null(ylim)){
+            ylim <- c(0, max(values)+(plot.range*text.gap))
+        }
+        xlim <- c(0, (max(coords)+0.5))
+    }
+    if(horiz){
+        if(is.null(ylim)){
+            xlim <- c(0, max(values)+(plot.range*text.gap))
+        }
+        ylim <- c(0, (max(coords)+0.5))
+        
+        
     }
 
     a <- barplot(values, xlab = xlab, ylab = ylab, main = main, col = col, 
