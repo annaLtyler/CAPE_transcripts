8cce491123efe2de21db147364bbcd0631a10281
annatyler <anna.l.tyler@gmail.com>
2022-04-29 10:48:54 -0400

    updates to best penalty selection
    Added a way to skew toward a harsher or more lenient penalty in the case of multiple equal penalties

--- Code/perm_best_results.R
+++ Code/perm_best_results.R
@@ -1,29 +1,48 @@
+#This function takes results from get_perm_grid()
+#and returns the best CCA penalty based on p 
+#values and the difference in canonical correlation
+#between the experimental canonical correlations
+#and the null canonical correlations. The penalties
+#with the maximum difference in correlations are 
+#returned. 
+#the skew.toward parameters indicate whether to skew 
+#toward harsher penalties (0), or more lenient penalties (1)
+#if there are multiple best penalty pairs. The x and z
+#allow skewing on both the x and the z axes independently.
+#These parameters are only implemented if return.top.only
+#is TRUE. Otherwise all best penalties are returned.
+
 perm_best_results <- function(perm_grid, pval.thresh = 0.05, 
-    cor.diff.thresh = 0, return.top.only = FALSE, 
-    plot.results = TRUE, row.text.shift = -0.25, col.text.shift = 0){
+    return.top.only = FALSE, 
+    plot.results = TRUE, row.text.shift = -0.25, col.text.shift = 0,
+    skew.toward.x = 0, skew.toward.z = 0){
 
     pass.p <- which(perm_grid$p <= pval.thresh)
     cor.diff <- perm_grid$Cor - perm_grid$Perm.Cor
-    pass.cor <- which(cor.diff > cor.diff.thresh)
-    
-    if(return.top.only){
-        best.penalty.idx <- intersect(which.max(cor.diff), pass.p)
-    }else{
-        best.penalty.idx <- intersect(pass.p, pass.cor)
+    max.cor.diff <- floor(max(cor.diff)*100)/100
+    pass.cor <- which(cor.diff >= max.cor.diff)
+
+    best.penalty.idx <- intersect(pass.p, pass.cor)
+
+    if(length(best.penalty.idx) > 1 && return.top.only){ #pick the best penalty skewing toward harsher or more lenient depending on skew.toward.x and skew.toward.z
+        penalty.row.col <- idx_to_row_col(best.penalty.idx, nrow(cor.diff))
+        if(length(unique(penalty.row.col[,1])) > 1){
+            best.row.idx <- get.nearest.pt(penalty.row.col[,1]/nrow(cor.diff), skew.toward.x)
+        }else{
+            best.row.idx <- 1
+        }
+        if(length(unique(penalty.row.col[,2])) > 1){
+            best.col.idx <- get.nearest.pt(penalty.row.col[,2]/ncol(cor.diff), skew.toward.z)
+        }else{
+            best.col.idx <- 1
+        }
+        best.penalty.idx <- row_col_to_idx(penalty.row.col[best.row.idx, 1], penalty.row.col[best.col.idx,2], nrow(cor.diff))
     }
     
-    nx <- nrow(cor.diff)
-    nz <- ncol(cor.diff)    
-
-    #which rows correspond to the indices found
-    best.penalty.x <- best.penalty.idx %% nx
-    best.penalty.x[which(best.penalty.x == 0)] <- nx
-
-    #which columns correspond to the indices found
-    best.penalty.z <- ceiling(best.penalty.idx/nx)
+    best.penalty.row.col <- idx_to_row_col(best.penalty.idx, nrow(cor.diff))
     
-    best.penalty <- list("x" = as.numeric(rownames(cor.diff)[best.penalty.x]),
-        "z" = as.numeric(colnames(cor.diff)[best.penalty.z]))
+    best.penalty <- list("x" = as.numeric(rownames(cor.diff)[best.penalty.row.col[,1]]),
+        "z" = as.numeric(colnames(cor.diff)[best.penalty.row.col[,2]]))
 
     star.nudge = 0.3
 
