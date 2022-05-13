564bd6523871cabe840120ec51e5ff1c27c756cb
annatyler <anna.l.tyler@gmail.com>
2022-05-13 14:40:35 -0400

    more som code

---
+++ Code/plot_portrait.R
@@ -0,0 +1,23 @@
+#byrow indicates whether to build the matrix of values by row (TRUE)
+#or by column (FALSE), and rotate is a numeric value 0, 1, 2, or 3
+#indicating how many times the matrix should be rotated before displaying.
+
+plot_portrait <- function(portraitV, global.min = NULL, global.max = NULL, main = "", 
+  byrow = FALSE, rotate = 0){
+  
+  portraitV[which(is.na(portraitV))] <- 0
+  map.dim <- sqrt(length(portraitV))
+  map.mat <- matrix(portraitV, nrow = map.dim, ncol = map.dim, byrow = byrow)
+  if(rotate > 0){
+    for(i in 1:rotate){
+      map.mat <- rotate.mat(map.mat)
+    }
+  }
+  if(!is.null(global.min)){
+    imageWithText(map.mat, show.text = FALSE, use.pheatmap.colors = TRUE, 
+      global.color.scale = TRUE, global.min = global.min, global.max = global.max, 
+      main = main)  
+  }else{
+    imageWithText(map.mat, show.text = FALSE, use.pheatmap.colors = TRUE, main = main)
+  }
+}
--- Documents/1.Setup_Data/test.R
+++ Documents/1.Setup_Data/test.R
@@ -1,4 +1,15 @@
-CCA(matched.mats[[i]]$X[,ordered.transcripts[[i]]], 
-                matched.mats[[i]]$Z, typex = "standard", typez = "standard", 
-                K = num.components, penaltyx = 1, penaltyz = 1, chromx = 1, 
-                trace = FALSE)
+env.names <- names(env)
+
+search.term = "tree"
+
+for(i in 1:length(env.names)){
+    test  <- get(env.names[i], envir = env)
+    test.class <- class(test)
+    if(test.class[1] == "list"){
+        test.names <- names(test)
+        term.idx <- grep(search.term, test.names)
+        #test.names[term.idx]
+        found.term <- as.logical(length(term.idx))
+        if(found.term){print(paste(i, env.names[i]))}
+    }
+}
