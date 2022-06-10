903bf1cf5ee6a25c22f1d7f4895453b7627d6d50
annatyler <48364995+annaLtyler@users.noreply.github.com>
2022-06-10 13:24:55 -0400

    updates

--- Code/plot_portrait.R
+++ Code/plot_portrait.R
@@ -3,7 +3,7 @@
 #indicating how many times the matrix should be rotated before displaying.
 
 plot_portrait <- function(portraitV, global.min = NULL, global.max = NULL, main = "", 
-  byrow = FALSE, rotate = 0){
+  byrow = FALSE, rotate = 0, show.axes = FALSE, show.text = FALSE){
   
   portraitV[which(is.na(portraitV))] <- 0
   map.dim <- sqrt(length(portraitV))
@@ -14,10 +14,17 @@ plot_portrait <- function(portraitV, global.min = NULL, global.max = NULL, main 
     }
   }
   if(!is.null(global.min)){
-    imageWithText(map.mat, show.text = FALSE, use.pheatmap.colors = TRUE, 
+    imageWithText(map.mat, show.text = show.text, use.pheatmap.colors = TRUE, 
       global.color.scale = TRUE, global.min = global.min, global.max = global.max, 
       main = main)  
   }else{
-    imageWithText(map.mat, show.text = FALSE, use.pheatmap.colors = TRUE, main = main)
+    imageWithText(map.mat, show.text = show.text, use.pheatmap.colors = TRUE, main = main)
   }
+
+  if(show.axes){
+    axis(1, at = seq(0,ncol(map.mat), 5))
+    axis(2, at = seq(0,nrow(map.mat), 5))
+  }
+
+  invisible(map.mat)
 }
