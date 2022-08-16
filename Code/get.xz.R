618579ecdc627e2fceb156f92919f3f29419ef24
annatyler <48364995+annaLtyler@users.noreply.github.com>
2022-08-16 10:13:04 -0400

    added code for query cape

--- Code/get.xz.R
+++ Code/get.xz.R
@@ -6,8 +6,8 @@ get.xz  <- function(x.mat, z.mat){
     common.x.locale <- match(common.ind, rownames(x.mat))
     common.z.locale <- match(common.ind, rownames(z.mat))
 
-    ordered.x <- x.mat[common.x.locale,]
-    ordered.z  <- z.mat[common.z.locale,]
+    ordered.x <- x.mat[common.x.locale,,drop=FALSE]
+    ordered.z  <- z.mat[common.z.locale,,drop=FALSE]
 
     x.na.locale <- which(is.na(ordered.x), arr.ind = TRUE)
     z.na.locale <- which(is.na(ordered.z), arr.ind = TRUE)
@@ -15,10 +15,10 @@ get.xz  <- function(x.mat, z.mat){
     if(length(x.na.locale) > 0 | length(z.na.locale) > 0){
         na.locale <- rbind(x.na.locale, z.na.locale)
         if(nrow(na.locale) > 0){
-            ordered.x <- ordered.x[-na.locale[,1],]
+            ordered.x <- ordered.x[-na.locale[,1],,drop=FALSE]
         }
         if(nrow(na.locale) > 0){
-            ordered.z <- ordered.z[-na.locale[,1],]
+            ordered.z <- ordered.z[-na.locale[,1],,drop=FALSE]
         }
     }
 
